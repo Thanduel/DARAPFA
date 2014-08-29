aac7930a6b729dbcc20d7f395a90e585424fc2f0
Dave <mynaamisdavid@gmail.com>
2014-08-30 00:29:07 +0200

    Remove Test.java Update Map.java to include main function to test MapReader.java

--- src/map/Map.java
+++ src/map/Map.java
@@ -2,14 +2,14 @@ package map;
 
 public class Map {
 
-	private int sizeOfMapSideInPatches;
+	private int sizeOfMapSideInTiles;
 	
 	private MapTileCorner[][] mapTileCornerArray;
 	
 	Map(int sizeOfMapSideInPatches){
-		this.sizeOfMapSideInPatches = sizeOfMapSideInPatches;
+		sizeOfMapSideInTiles = (sizeOfMapSideInPatches * 16) + 1;
 
-		mapTileCornerArray = new MapTileCorner[sizeOfMapSideInPatches][sizeOfMapSideInPatches];
+		mapTileCornerArray = new MapTileCorner[sizeOfMapSideInTiles][sizeOfMapSideInTiles];
 	}
 
 	public void setMapTileCorner(MapTileCorner mapTileCorner){
@@ -18,6 +18,24 @@ public class Map {
 	
 	public MapTileCorner getMapTileCorner(int xIndex, int yIndex){
 		return mapTileCornerArray[xIndex][yIndex];
+	}
+	
+	public static void main(String[] args){
+		MapTileCorner tempTile;
+		MapReader rawr = new MapReader("C:/Users/FNB/git/0ad/0ad/binaries/data/mods/public/maps/scenarios/Arcadia 02.pmp", "placeholder");
+		//MapReader rawr = new MapReader("Arcadia 02.pmp", "placeholder");
+		Map map = new Map(rawr.getNumMapTileCorners());
+
+		tempTile = rawr.getNextMapTileCorner();
+		map.setMapTileCorner(tempTile);
+
+		while(tempTile != null){
+			tempTile = rawr.getNextMapTileCorner();
+			map.setMapTileCorner(tempTile);
+			System.out.println(tempTile.getxIndex());
+			System.out.println(tempTile.getyIndex());
+			System.out.println(tempTile.getHeight());
+		}
 	}
 	
 }
--- src/map/MapReader.java
+++ src/map/MapReader.java
@@ -1,6 +1,130 @@
 package map;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+//import java.util.Scanner;
+
 public class MapReader {
 	
+	
+	FileInputStream fileInput;
+//	Scanner fileInput;
+	private int mapSize;
+	private int xIndex;
+	private int yIndex;
+	private int counter;
+	private int maxNumCorners;
+	
+	MapReader(String pmpPath, String xmlPath)
+	{
+		xIndex = 0;
+		yIndex = 0;
+		counter = 0;
+
+		System.out.println(pmpPath);
+		//try{
+	//		inputStream = new ObjectInputStream(new FileInputStream(pmpPath));
+	//	}catch(IOException e){
+	//		System.out.println("I don't actually care about this exception" + e);
+	//	}
+		
+		try{
+		fileInput = new FileInputStream(pmpPath);
+		}catch(IOException e){
+			System.out.println("I don't actually care about this exception" + e);
+		}
+		readMapSize();
+		maxNumCorners = ((mapSize * 16) + 1) * ((mapSize * 16) + 1);
+	}
+	
+	public int getNumMapTileCorners(){
+		return mapSize + 1;
+	}
+	private int little2big(int i) {
+	    return((i&0xff)<<24)+((i&0xff00)<<8)+((i&0xff0000)>>8)+((i>>24)&0xff);
+	}
+	
+	private int readInt(){
+		try{
+		int tempInt = fileInput.read();
+		tempInt = tempInt << 8;
+		tempInt = tempInt | fileInput.read();
+		tempInt = tempInt << 8;
+		tempInt = tempInt | fileInput.read();
+		tempInt = tempInt << 8;
+		tempInt = tempInt | fileInput.read();
+		return little2big(tempInt);
+		}catch(IOException e){
+			System.out.println("I don't actually care about this exception" + e);
+			return -1;
+		}
+	}
+	
+	private short readShort(){
+		try{
+		int tempShort = fileInput.read();
+		tempShort = tempShort | (fileInput.read() << 8);
+		return (short)tempShort;
+		}catch(IOException e){
+			System.out.println("I don't actually care about this exception" + e);
+			return -1;
+		}
+	}
+
+	private char readChar(){
+		try{
+		int tempChar = fileInput.read();
+		return (char)tempChar;
+		}catch(IOException e){
+			System.out.println("I don't actually care about this exception" + e);
+			return (char)-1;
+		}
+	}
+	
+	private void moveFilePointerToStartofMapSize()
+	{
+		readInt();
+		readInt();
+		readInt();
+	}
+	
+	private int nextXIndex(){
+		int oldIndex = xIndex;
+		xIndex++;
+
+		if(xIndex == mapSize + 1)
+			xIndex = 0;
+
+		return oldIndex; 
+	}
+
+	private int nextYIndex(){
+		int oldIndex = yIndex;
+		yIndex++;
+
+		if(yIndex == mapSize + 1)
+			yIndex = 0;
+
+		return oldIndex; 
+	}
+	
+	private void readMapSize()
+	{
+		moveFilePointerToStartofMapSize();
+		mapSize = readInt();
+		System.out.println(mapSize);
+	}
+	
+	public MapTileCorner getNextMapTileCorner(){
+		if(counter >= maxNumCorners)
+			return null;
+
+		 counter++;
+		 return new MapTileCorner(readShort(), nextXIndex(), nextYIndex());
+	}
 
 }
