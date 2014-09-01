f0e91b71356db54658bdb4c3d2d29d3efb8ab751
Dave <mynaamisdavid@gmail.com>
2014-09-02 00:05:17 +0200

    add Entity data holder class and start of XML parser

---
+++ src/map/Entity.java
@@ -0,0 +1,13 @@
+package map;
+
+public class Entity {
+
+  private int uid;
+  private float x, y, theta;
+  private String template;
+
+	Entity(int uid, float x, float y, float theta, String template_path)
+  {
+
+  }
+}
