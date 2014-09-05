7881f497a7315ce38ba506315d5253793d339ee3
Roland <elliott.roland@gmail.com>
2014-09-06 00:50:14 +0200

    Add Obstruction.getObstructionFromEntity method
    Also, add Logger class
    Also, add the ParsingException RuntimeException

--- src/map/Obstruction.java
+++ src/map/Obstruction.java
@@ -1,8 +1,32 @@
 package map;
 
 import java.awt.Shape;
+import java.awt.geom.Point2D;
 
 public abstract class Obstruction {
 	
 	public abstract Shape getShape();
+	
+	public static Obstruction getObstructionFromEntry(Entity e)	{
+		TemplateXmlParser txmlp = new TemplateXmlParser(e);
+		if(txmlp.isObstruction())	{
+			Point2D.Float location = txmlp.getObstructionLocation();
+			if(txmlp.isSquare())	{
+				return new SquareObstruction(location.x,
+										location.y,
+										txmlp.getObstructionWidth(),
+										txmlp.getObstructionHeight(),
+										e.getTheta());
+			}
+			else if(txmlp.isCircle())	{
+				return new CircleObstruction(location.x,
+											location.y,
+											txmlp.getObstructionRadius());
+			}
+			else	{
+				throw new ParsingException("Entity needs to be either a Cirlce or a Square");
+			}
+		}
+		return null;
+	}
 }
