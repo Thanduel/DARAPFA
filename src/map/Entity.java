45240d0a1f326b765fa3b3fea83f6ab27272b978
Dave <mynaamisdavid@gmail.com>
2014-09-05 20:29:11 +0200

    Update Entity Class

--- src/map/Entity.java
+++ src/map/Entity.java
@@ -4,10 +4,54 @@ public class Entity {
 
   private int uid;
   private float x, y, theta;
-  private String template;
+  private String template_path;
 
-	Entity(int uid, float x, float y, float theta, String template_path)
+  Entity(int uid, float x, float y, float theta, String template_path)
   {
-
+		this.uid = uid;
+		this.x = x;
+		this.y = y;
+		this.theta = theta;
+		this.template_path = template_path;
   }
+
+public int getUid() {
+	return uid;
+}
+
+public void setUid(int uid) {
+	this.uid = uid;
+}
+
+public float getX() {
+	return x;
+}
+
+public void setX(float x) {
+	this.x = x;
+}
+
+public float getY() {
+	return y;
+}
+
+public void setY(float y) {
+	this.y = y;
+}
+
+public float getTheta() {
+	return theta;
+}
+
+public void setTheta(float theta) {
+	this.theta = theta;
+}
+
+public String getTemplate_path() {
+	return template_path;
+}
+
+public void setTemplate_path(String template_path) {
+	this.template_path = template_path;
+}
 }
